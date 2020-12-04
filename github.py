900bbcab1ecdea4122eb5d60403c45cb1b80d743
https://github.com/DunnCreativeSS/-updateAllYourGitReposWithFundingFile <jarettrsdunn+git@gmail.com>
2020-12-04 02:59:58 -0400

    whoops

--- github.py
+++ github.py
@@ -5,5 +5,5 @@ import requests
 r = requests.get('https://api.github.com/users/dunncreativess/repos').json()
 repos = []
 for repo in r:
-    repos.append(('https://dunncreativess:aa2afcf2fac47be6010d717363d4de628e4c43a7@github.com/dunncreativess/' + repo['name']))
+    repos.append(('https://dunncreativess:{personalaccesstoken}@github.com/dunncreativess/' + repo['name']))
 print(repos)
